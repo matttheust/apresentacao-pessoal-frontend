0944edaa530c381285645127cc11cc8a8069007d
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2024-10-07 13:48:47 -0300

    update: detalhes visuais - hover e cores dos cards

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import projetosData from '../components/projetosData'; // Importando os dados dos projetos
-
+import projetosData from './projetosData'; // Importando os dados dos projetos
 
 const Projetos = () => {
   const projects = projetosData; // Usando os dados importados
@@ -11,7 +10,11 @@ const Projetos = () => {
       <h2 className="text-2xl font-bold mb-4">Meus Projetos</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.map((project) => (
-          <Link to={`/project/${project.id}`} key={project.id} className="block p-4 border rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+          <Link 
+            to={`/project/${project.id}`} 
+            key={project.id} 
+            className="block p-4 border rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl bg-white hover:bg-gray-200" // Ajuste aqui
+          >
             <div className="card-content">
               <h3 className="text-xl font-semibold">{project.name}</h3>
               <p className="text-gray-600">{project.description}</p>
--- src/components/projetosData.js
+++ src/components/projetosData.js
@@ -1,38 +1,37 @@
 const projetosData = [
   {
     id: 1,
-    name: 'Projeto de Portfólio',
-    description: 'Um portfólio pessoal para exibir meus projetos e habilidades.',
-    details: 'Este projeto foi desenvolvido usando React e Tailwind CSS. Inclui uma seção de contato e uma galeria de projetos.',
-    technologies: 'React, Tailwind CSS, JavaScript',
-    repositoryLink: 'https://github.com/seuusuario/projeto-portfolio',
-    demoLink: 'https://seuusuario.github.io/projeto-portfolio',
-    creationDate: '2023-01-15',
-    image: 'https://via.placeholder.com/400', // URL da imagem do projeto
-  },
-  {
-    id: 2,
     name: 'Aplicativo de Tarefas',
-    description: 'Um aplicativo para gerenciar tarefas diárias com funcionalidades de adicionar, editar e remover tarefas.',
+    description: 'Este é um simples aplicativo de lista de tarefas desenvolvido usando React.js e Next.js. Ele permite que os usuários adicionem, editem e marquem tarefas como concluídas.',
     details: 'Desenvolvido com React e Redux, este aplicativo permite que os usuários organizem suas tarefas de forma eficiente.',
     technologies: 'React, Redux, CSS',
-    repositoryLink: 'https://github.com/seuusuario/app-tarefas',
-    demoLink: 'https://seuusuario.github.io/app-tarefas',
+    repositoryLink: 'https://github.com/matttheust/to-do-list',
+    demoLink: 'https://matttheus.github.io/app-tarefas',
     creationDate: '2023-02-10',
     image: 'https://via.placeholder.com/400',
   },
   {
-    id: 3,
-    name: 'Clone do Instagram',
-    description: 'Um clone simples do Instagram, permitindo que os usuários publiquem fotos e sigam uns aos outros.',
-    details: 'Utiliza Firebase para autenticação e armazenamento de dados. O front-end é construído com React e CSS.',
-    technologies: 'React, Firebase, CSS',
-    repositoryLink: 'https://github.com/seuusuario/clone-instagram',
-    demoLink: 'https://seuusuario.github.io/clone-instagram',
+    id: 2,
+    name: 'Previsão do Tempo Local',
+    description: 'Esta é uma aplicação Angular simples para fornecer previsões do tempo locais.',
+    details: 'O front-end é construído com React e CSS. Usa a API da OpenWeatherMap para obter as previsões do tempo.',
+    technologies: 'React, Firebase, CSS, OpenWeatherMap API',
+    repositoryLink: 'https://github.com/matttheust/previsao-tempo-angular',
+    demoLink: 'https://matttheus.github.io/weather-app',
     creationDate: '2023-03-05',
     image: 'https://via.placeholder.com/400',
   },
-  // Adicione mais projetos conforme necessário
+{
+    id: 3,
+    name: 'QR Genius',
+    description: 'QR Genius é uma aplicação mobile para criação de QR Codes.',
+    details: 'Desenvolvido em Swift, permite que o usuário crie QR Codes personalizados para compartilhamento e marketing.',
+    technologies: 'Swift, UIKit, CoreImage',
+    repositoryLink: 'https://github.com/matttheust/GeniusQR',
+    demoLink: 'https://github.com/matttheust/qr-genius-mobile',
+    creationDate: '2024-02-14',
+    image: 'https://via.placeholder.com/400',
+}
 ];
 
 export default projetosData;
